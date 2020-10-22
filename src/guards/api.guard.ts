62eda8820d49525231f997b871d81c546398e77e
Pavel Krcmar <chilly@email.cz>
2020-10-22 11:49:16 +0200

    print headers

--- src/guards/api.guard.ts
+++ src/guards/api.guard.ts
@@ -9,6 +9,7 @@ export class ApiGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
     const key1 = context.switchToHttp().getRequest<Request>()?.headers?.['x-api-key'];
     const key2 = this.configService.get('X-API-KEY1');
+    console.log(context.switchToHttp().getRequest<Request>()?.headers);
     console.log(key1, key2);
     return key1 === key2;
   }
